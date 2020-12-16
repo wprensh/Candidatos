5399688082b3bd6a17e842d5e0c4a1128ea52031
Walter Prens Herrera <wprensh@hotmail.com>
2020-12-16 09:33:34 -0600

    Editar Entrevista
    Pantalla Editar Entrevista

--- PruebaTecnicaSolution/PruebaTecnica/Controllers/HomeController.cs
+++ PruebaTecnicaSolution/PruebaTecnica/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using PruebaTecnicaLibrary.PruebaTecnicaService;
+﻿using PruebaTecnica.ServiceReference;
+using PruebaTecnicaLibrary.PruebaTecnicaService;
 using PruebaTecnicaLibrary.Vo;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ namespace PruebaTecnica.Controllers
     public class HomeController : Controller
     {
         PruebaTecnicaDb context = new PruebaTecnicaDb();
+        PtWcfClient ptWcfClient = new PtWcfClient();
         DatosAuxiliares datosAuxiliares = new DatosAuxiliares();
 
         public ActionResult Index()
@@ -35,8 +37,8 @@ namespace PruebaTecnica.Controllers
         public ActionResult Pruebas()
         {
             List<Entrevista> entrevistas = new List<Entrevista>();
-            
-            PruebaTecnicaLibrary.PruebaTecnicaService.Json_Service json_Service = new PruebaTecnicaLibrary.PruebaTecnicaService.Json_Service();
+
+            Json_Service json_Service = new Json_Service();
             List<MyJsonData> myJsonDatas = json_Service.GetReleases("http://jsonplaceholder.typicode.com");
             foreach (var item in context.Entrevistas)
             {
@@ -66,11 +68,11 @@ namespace PruebaTecnica.Controllers
             //                 entreview.Tipo_Tecnologia,
             //                 candidatos.name
             //             };
-            
+
             return View(entrevistas);
         }
 
-      
+
 
         /// <summary>  
         /// This method will return PartialView with Employee Model  
@@ -79,20 +81,48 @@ namespace PruebaTecnica.Controllers
         /// <returns></returns>  
         public PartialViewResult getByIdTecnologia(int idTegnologia)
         {
-            ViewBag.LstTipoTecnologia = datosAuxiliares.tecnologias.Where(s=>s.Id == idTegnologia).Select(m=>m.tipoTecnologias).ToList();
-            
+            ViewBag.LstTipoTecnologia = datosAuxiliares.tecnologias.Where(s => s.Id == idTegnologia).Select(m => m.tipoTecnologias).ToList();
+
             return PartialView("_getByIdTecnologia");
         }
 
-        
 
-        public ActionResult AddPruebas() {
+
+        public ActionResult AddPruebas()
+        {
             ViewBag.Accounts = datosAuxiliares.tecnologias;
-            Entrevista entrevista_obj = new Entrevista() {
+            Entrevista entrevista_obj = new Entrevista()
+            {
                 reclutadores = datosAuxiliares.GetReclutadores()
             };
 
             return View(entrevista_obj);
+        }
+
+        [HttpPost]
+        public ActionResult AddPruebas(Entrevista entrevista)
+        {            
+            if (ptWcfClient.NuevaEntrevista(entrevista) == true)
+            {
+                return Redirect("Pruebas");
+            }
+            return View(entrevista);
+            
+        }
+
+        public ActionResult ModifyPruebas(int id) {
+            Entrevista entrevista = context.Entrevistas.Where(e => e.Id == id).FirstOrDefault();
+            entrevista.reclutadores = datosAuxiliares.GetReclutadores();
+            return View(entrevista);
+        }
+
+        [HttpPost]
+        public ActionResult ModifyPruebas(Entrevista entrevista) {
+            if (ptWcfClient.EditarEntrevista(entrevista) == true)
+            {
+                return Redirect("Pruebas");
+            }
+            return View(entrevista);
         }
     }
 }
